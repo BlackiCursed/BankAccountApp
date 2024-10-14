341a6b5c0c772afeebfa6b472c4bf17d2974a9dd
Blacki <Blacki@DESKTOP-70TL495>
2024-10-15 03:57:16 +0530

    Add project files.

---
+++ BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountApp
+{
+    public class BankAccount
+    {
+        private int accountNumber;
+        private string accountHolder;
+        public string AccountHolder
+        {
+            get { return accountHolder; }
+            set { accountHolder = value; }
+        }            
+
+        private double balance;
+        public double Balance
+        {
+            get { return balance; }
+            set { balance = value; }
+        }
+        private int depositMoney;
+        public void DepositMoney(double amount)
+        {
+            if (depositMoney > 0)
+            {
+                balance += amount;
+            }
+        }
+        private double withdrawMoney;
+        public void WithdrawMoney(double amount)
+        {
+           if (amount <= 0)
+            {  
+                Console.WriteLine($"Amount must be greater than zero");   
+                return; 
+            }
+            if (amount > balance)
+            {
+                Console.WriteLine("Insufficient Fund");
+                return;
+            }
+            
+        }
+
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using BankAccountApp;
+using System.Transactions;
+
+BankAccount b =  new BankAccount();
+b.AccountHolder = "Account Holder";
+b.Balance = 1234.12;
+b.WithdrawMoney (0);
+
+while (true)
+{
+    Console.WriteLine($"--- Welcome {b.AccountHolder} ---");
+    Console.WriteLine("1. Check Balance");
+    Console.WriteLine("2. Deposit Amount");
+    Console.WriteLine("3. Withdraw Amount");
+    Console.WriteLine("4. Exit");
+    Console.Write("Choose an Option");
+
+    string choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            Console.WriteLine($"Your Current balance is {b.Balance}");
+            break;
+        case "2":
+            Console.Write("Enter an amount: ");
+            if (Double.TryParse(Console.ReadLine(), out double DepositAmount))
+            {
+                Console.WriteLine(DepositAmount);
+                Console.WriteLine($"Your Updated Balance is: {DepositAmount + b.Balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Number");
+            }
+            break;
+        case "3":
+            Console.Write("Enter an Amount: ");
+            string input = Console.ReadLine();
+            if (double.TryParse(input, out double amount))
+            {
+                if (amount > b.Balance)
+                {
+                    Console.WriteLine("Invalid Amount or Insufficient Balance");
+                }
+                else
+                {
+                    Console.WriteLine($"Withdrawl Success Full Your Current Balance is {b.Balance - amount}");
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Amount");
+            }
+            break;
+            
+        case "4":
+            Console.WriteLine("Thank You For using our system");
+            return;
+        default:
+            Console.WriteLine("Invalid Choice");
+            break;            
+
+    }
+}
+
+
+
+
+
+
+
+
